6ce5af38e6e680c574d93e7044100635c70b3d30
Allen Shaw <allen@JoineryHQ.com>
2017-06-13 21:53:58 -0500

    Fix #5: correct kcfinder url.

--- ang/partials/rules.js
+++ ang/partials/rules.js
@@ -202,7 +202,7 @@
       };
 
       window.open(
-        '/sites/all/modules/civicrm/packages/kcfinder/browse.php?cms=civicrm&type=images',
+        CRM.config.resourceBase + "packages/kcfinder/browse.php?cms=civicrm&type=images",
         'kcfinder_textbox_image_url',
         'status=0, toolbar=0, location=0, menubar=0, directories=0, resizable=1, scrollbars=0, width=800, height=600'
       );
